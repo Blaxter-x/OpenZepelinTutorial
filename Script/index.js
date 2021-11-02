bd22fbb8009204ed6f75ecf96c8ebc67b14dce15
Andres Ulloa <andresguc@gmail.conm>
2021-11-02 11:04:30 -0500

    Code: Create a web interface
    - Create a index.js file to create a front-end application that
      query to dthe  Blockchain

---
+++ Script/index.js
@@ -0,0 +1,14 @@
+module.exports = async function main (callback) {
+    try {
+        // code
+
+        callback(0);
+        } catch (error) {
+            console.error(error);
+            callback(1);
+    }
+
+    // Retrieve accounts from the local node
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts)
+}
